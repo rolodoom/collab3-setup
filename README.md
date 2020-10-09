f15dfa66911da7e129fa19762cf20a028e935480
rolodoom <rolodoom@gmail.com>
2020-10-09 01:20:13 -0500

    Update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # collab3-setup
-Desktop icon for CollaB3 on Linux
+The missing desktop icon for [CollaB3](https://sampleson.com/collab3-free-tonewheel-organ.html) on Linux
+
+![Screenshot](screenshot.png "Screenshot")
+
+![Desktop Icon](local/share/icons/hicolor/256x256/apps/CollaB3.png "Desktop Icon")
+
+This installs the missing desktop icon for the Linux standalone audio app. The icon was extracted from the original *.exe installer.
+
+## Usage
+CollaB3 must be installed on $HOME/.sampleson/ folder.
+
+Clone repo and execute install script and follow the instructions.
+
+    git clone https://github.com/rolodoom/collab3-setup.git
+    cd collab3-setup
+    chmod +x main.sh && ./main.sh
---
+++ main.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#  _______   _______
+# |  _____| |  ___  |
+# | |       | |   | |    Rolando Ramos Torres (@rolodoom)
+# | |       | |___| |    http://rolandoramostorres.com
+# |_|       |_______|
+#  _         _______     collab3-setup/install.sh
+# | |       |  ___  |
+# | |       | |   | |    Missing desktop icon installer
+# | |_____  | |___| |    for CollaB3 software
+# |_______| |_______|    Tested on Kubuntu 20.04 LTS
+#
+#
+#
+#
+
+
+SAMPLESON=$(readlink -f "$HOME/.sampleson")
+
+# install subrutine
+aa_install () {
+    echo "installing desktop icon..."
+
+    # check if $HOME/.sampleson exists
+    if [ ! -d "$SAMPLESON" ]; then
+    # Create dir if doesn't exists
+        echo "The folder $SAMPLESON doesn't exist!!!"
+    else
+        # echo "The folder $SAMPLESON exists!!!"
+
+        # # copy .local files
+        cp -r local/share "$HOME/.local"
+
+    fi
+
+    echo "Done!!!"
+}
+
+# uninstall subrutine
+aa_uninstall () {
+    echo "Uninstalling desktop icon..."
+
+    # desktop files
+    rm -rf "$HOME/.local/share/applications/CollaB3.desktop"
+    # icons
+    rm -rf "$HOME/.local/share/icons/hicolor/256x256/apps/CollaB3.png"
+
+    echo "Done!!!"
+}
+
+
+# Start
+echo ""
+echo "*****************************************"
+echo "*    CollaB3 Desktop Icon Installer     *"
+echo "*****************************************"
+echo "Available commands:"
+echo ""
+echo "  [I]nstall"
+echo "  [U]ninstall"
+echo "  [Q]uit"
+echo ""
+echo -n "Command [I,U,Q]:"
+read input
+echo ""
+
+# 
+case "$input" in
+    install|INSTALL|I|i)
+    aa_install    
+    ;;
+
+    uninstall|UNINSTALL|U|u)
+    aa_uninstall
+    ;;
+      
+    quit|QUIT|Q|q|exit|EXIT|x|X) 
+    echo "Bye!!!"
+    ;;
+    
+    # NOT IMPLEMENTED
+    *)
+    echo "Choose an available command!!!"
+    ;;
+
+esac
